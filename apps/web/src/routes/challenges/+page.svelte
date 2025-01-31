10f7f16f91641b89ccdc6344f6a909d2c5c98263
joseph <joseph.surin@gmail.com>
2025-01-31 21:50:56 +1100

    load challs from api

--- apps/web/src/routes/challenges/+page.svelte
+++ apps/web/src/routes/challenges/+page.svelte
@@ -2,142 +2,51 @@
   import ChallengeBoard from "$lib/components/challenges/ChallengeBoard.svelte";
   import type { ChallengeCardProps } from "$lib/components/challenges/ChallengeCard.svelte";
   import ChallengeFilterer from "$lib/components/challenges/ChallengeFilterer.svelte";
+  import api, { wrapLoadable } from "$lib/api/index.svelte";
+  import {
+    getCategoriesFromTags,
+    getDifficultyFromTags,
+  } from "$lib/utils/challenges";
 
-  let allChallenges: ChallengeCardProps[] = [
-    {
-      title: "babypwn",
-      categories: ["pwn"],
-      solves: 23,
-      points: 190,
-      isSolved: true,
-      difficulty: "easy",
-    },
-    {
-      title: "lots of challs",
-      categories: ["crypto"],
-      solves: 1,
-      points: 488,
-      isSolved: true,
-      difficulty: "hard",
-    },
-    {
-      title: "chal with a slightly longer title",
-      categories: ["pwn", "crypto"],
-      solves: 1,
-      points: 500,
-      isSolved: false,
-      difficulty: "easy",
-    },
-    {
-      title: "puzl",
-      categories: ["misc", "crypto"],
-      solves: 1,
-      points: 488,
-      isSolved: true,
-      difficulty: "hard",
-    },
-    {
-      title: "reeev",
-      categories: ["rev", "crypto"],
-      solves: 112,
-      points: 95,
-      isSolved: true,
-      difficulty: "medium",
-    },
-    {
-      title: "crycry",
-      categories: ["crypto"],
-      solves: 11,
-      points: 333,
-      isSolved: false,
-      difficulty: "easy",
-    },
-    {
-      title: "web1",
-      categories: ["web"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "easy",
-    },
-    {
-      title: "web2",
-      categories: ["web"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "medium",
-    },
-    {
-      title: "web3",
-      categories: ["web"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "medium",
-    },
-    {
-      title: "web4",
-      categories: ["web"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "beginner",
-    },
-    {
-      title: "web5",
-      categories: ["web"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "beginner",
-    },
-    {
-      title: "hello",
-      categories: ["web", "pwn", "crypto", "misc"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "hard",
-    },
-    {
-      title: "goodbye",
-      categories: ["rev", "web", "pwn", "crypto", "misc"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "hard",
-    },
-    {
-      title: "web6",
-      categories: ["web"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "hard",
-    },
-    {
-      title: "web7",
-      categories: ["web"],
-      solves: 0,
-      points: 500,
-      isSolved: false,
-      difficulty: "hard",
-    },
-  ];
+  let apiChallenges = wrapLoadable(api.GET("/challenges"));
 
-  let challenges = $state(allChallenges.sort((a, b) => b.solves - a.solves));
+  let allChallenges: ChallengeCardProps[] | undefined = $derived(
+    apiChallenges.r?.data
+      ? apiChallenges.r?.data.data.challenges.map((c) => ({
+          title: c.title,
+          categories: getCategoriesFromTags(c.tags),
+          solves: c.solve_count,
+          points: c.score!,
+          isSolved: c.solved_by_me,
+          difficulty: getDifficultyFromTags(c.tags),
+        }))
+      : undefined,
+  );
+
+  let challenges: ChallengeCardProps[] | undefined = $state();
+  $effect(() => {
+    if (allChallenges) {
+      challenges = allChallenges.sort((a, b) => b.solves - a.solves);
+    }
+  })
 </script>
 
 <div class="w-11/12 m-auto h-auto mt-8">
   <div class="self-center text-center text-4xl font-black pb-4">Challenges</div>
-  <div class="grid grid-cols-[min(25%,20rem)_75%] gap-4 px-16 py-8">
-    <div class="mt-8">
-      <ChallengeFilterer
-        challenges={allChallenges}
-        onFilter={(res) => (challenges = res)}
-      />
+  {#if apiChallenges.loading}
+    <div class="flex flex-col items-center gap-4 mt-16">
+        <div class="loading loading-spinner loading-lg text-primary"></div>
+        <p class="text-center">Loading challenges...</p>
     </div>
-    <ChallengeBoard {challenges} />
-  </div>
+  {:else}
+    <div class="grid grid-cols-[min(25%,20rem)_75%] gap-4 px-16 py-8">
+      <div class="mt-8">
+        <ChallengeFilterer
+          challenges={allChallenges!}
+          onFilter={(res) => (challenges = res)}
+        />
+      </div>
+      <ChallengeBoard challenges={challenges!} />
+    </div>
+  {/if}
 </div>
