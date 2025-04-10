f5d1513fd1d69bc720bc6822ad41ba84a7236387
tom <hi@tomn.me>
2025-04-10 21:08:22 +1000

    add token table and team tag table

---
+++ migrations/1744281954832_session_and_lock.ts
@@ -0,0 +1,56 @@
+import { sql, type Kysely } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  const schema = db.schema;
+  await schema
+    .createTable("lock")
+    .addColumn("key", "varchar", (col) => col.notNull().primaryKey())
+    .addColumn("value", "varchar", (col) => col.notNull())
+    .addColumn("created_at", "timestamptz", (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .execute();
+
+  await schema
+    .createTable("session")
+    .addColumn("id", "integer", (col) =>
+      col.primaryKey().generatedByDefaultAsIdentity(),
+    )
+    .addColumn("hash", "bytea", (col) => col.notNull().unique())
+    .addColumn("user_id", "integer", (col) =>
+      col.notNull().references("user.id").onDelete("cascade"),
+    )
+    .addColumn("type", "varchar(32)", (col) => col.notNull())
+    .addColumn("data", "jsonb", (col) => col.notNull().defaultTo("{}"))
+    .addColumn("ip", "varchar(64)")
+    .addColumn("client", "varchar(64)")
+    .addColumn("created_at", "timestamptz", (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn("refreshed_at", "timestamptz", (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn("expires_at", "timestamptz", (col) => col.notNull())
+    .execute();
+  await schema
+    .createIndex("session_idx_user_id_type")
+    .on("session")
+    .columns(["user_id", "type"])
+    .execute();
+  await schema
+    .createIndex("session_idx_user_id_client")
+    .on("session")
+    .columns(["user_id", "client"])
+    .execute();
+  await schema
+    .createIndex("session_idx_expires_at")
+    .on("session")
+    .expression(sql`expires_at DESC`)
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  const schema = db.schema;
+  await schema.dropTable("session").execute();
+  await schema.dropTable("lock").execute();
+}
