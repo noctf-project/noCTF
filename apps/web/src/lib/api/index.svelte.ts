0bbf6a7bf446aa5ed8002436d37febcdc65d05c9
tom <hi@tomn.me>
2025-04-11 08:36:12 +0000

    Inject URL using env var

--- apps/web/src/lib/api/index.svelte.ts
+++ apps/web/src/lib/api/index.svelte.ts
@@ -1,7 +1,7 @@
 import createClient from "openapi-fetch";
 import type { paths } from "@noctf/openapi-spec";
 
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 const client = createClient<paths>({
   baseUrl: API_BASE_URL,
   credentials: "include",
