de749f38c50ad19e48a3cc67d5ff10966a857743
tom <hi@tomn.me>
2025-07-08 22:08:51 +1000

    chore: remove comment

--- apps/web/src/lib/components/challenges/ChallengeCard.svelte
+++ apps/web/src/lib/components/challenges/ChallengeCard.svelte
@@ -23,8 +23,6 @@
   import DifficultyChip from "./DifficultyChip.svelte";
 
   const { data, onclick }: ChallengeCardProps = $props();
-  // Cannot destructure data here, breaks reactivity https://svelte.dev/docs/svelte/$state#Deep-state
-  // const { title, categories, solves, points, isSolved, difficulty } = data;
 </script>
 
 <button
