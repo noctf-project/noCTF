279d58f6308b5a227000a106f14c02247e583117
tom <hi@tomn.me>
2025-07-09 09:56:42 +1000

    rewrite api

--- apps/server/src/routes/admin_audit_log.ts
+++ apps/server/src/routes/admin_audit_log.ts
@@ -2,16 +2,10 @@ import { QueryAuditLogRequest } from "@noctf/api/requests";
 import { QueryAuditLogResponse } from "@noctf/api/responses";
 import type { FastifyInstance } from "fastify";
 import "@noctf/server-core/types/fastify";
-import type { Policy } from "@noctf/server-core/util/policy";
+import { Paginate } from "@noctf/server-core/util/paginator";
 
 export async function routes(fastify: FastifyInstance) {
   const { auditLogService } = fastify.container.cradle;
-
-  const auth = {
-    require: true,
-    scopes: new Set(["admin"]),
-    policy: ["AND", "admin.audit_log.get"] as Policy,
-  };
 
   fastify.post<{ Body: QueryAuditLogRequest; Reply: QueryAuditLogResponse }>(
     "/admin/audit_log/query",
@@ -19,13 +13,31 @@ export async function routes(fastify: FastifyInstance) {
       schema: {
         tags: ["admin"],
         security: [{ bearer: [] }],
-        auth,
+        auth: {
+          require: true,
+          scopes: new Set(["admin"]),
+          policy: ["admin.audit_log.get"],
+        },
         body: QueryAuditLogRequest,
         response: {
           200: QueryAuditLogResponse,
         },
       },
     },
-    async (request) => ({ data: await auditLogService.query(request.body) }),
+    async (request) => {
+      const { page, page_size, ...query } = request.body;
+      const [result, total] = await Promise.all([
+        Paginate(query, { page, page_size }, (q, l) =>
+          auditLogService.query(q, l),
+        ),
+        auditLogService.getCount(query),
+      ]);
+      return {
+        data: {
+          ...result,
+          total: total || result.entries.length,
+        },
+      };
+    },
   );
 }
