7e0ca5f98fb46923eaac28a12637a392e6db6711
tom <hi@tomn.me>
2025-07-11 07:07:17 +1000

    Push rate limits

--- apps/server/src/routes/team.ts
+++ apps/server/src/routes/team.ts
@@ -25,6 +25,7 @@ import { ActorType, TeamFlag } from "@noctf/server-core/types/enums";
 import { Policy } from "@noctf/server-core/util/policy";
 import SingleValueCache from "@noctf/server-core/util/single_value_cache";
 import { Paginate } from "@noctf/server-core/util/paginator";
+import { GetRouteUserIPKey } from "@noctf/server-core/util/limit_keys";
 
 export async function routes(fastify: FastifyInstance) {
   const adminPolicy: Policy = ["admin.team.get"];
@@ -177,6 +178,13 @@ export async function routes(fastify: FastifyInstance) {
           require: true,
           policy: ["team.self.join"],
         },
+        rateLimit: (r) => [
+          {
+            key: GetRouteUserIPKey(r),
+            limit: 4,
+            windowSeconds: 60,
+          },
+        ],
         body: JoinTeamRequest,
         response: {
           201: MeTeamResponse,
@@ -205,6 +213,13 @@ export async function routes(fastify: FastifyInstance) {
           require: true,
           policy: ["team.self.leave"],
         },
+        rateLimit: (r) => [
+          {
+            key: GetRouteUserIPKey(r),
+            limit: 1,
+            windowSeconds: 60,
+          },
+        ],
       },
     },
     async () => {
@@ -248,6 +263,13 @@ export async function routes(fastify: FastifyInstance) {
           require: true,
           policy: ["team.self.update"],
         },
+        rateLimit: (r) => [
+          {
+            key: GetRouteUserIPKey(r),
+            limit: 3,
+            windowSeconds: 60,
+          },
+        ],
         body: UpdateTeamRequest,
         response: {
           200: UpdateTeamResponse,
