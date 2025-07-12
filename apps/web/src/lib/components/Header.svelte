6970c96025c9ba3ba0f3e84f5d629f61b459ec88
Sam Calamos <sam.calamos@gmail.com>
2025-07-12 13:19:40 +1000

    remove extra div causing dumb issue

--- apps/web/src/lib/components/Header.svelte
+++ apps/web/src/lib/components/Header.svelte
@@ -34,24 +34,24 @@
             d="M4 6h16M4 12h8m-8 6h16"
           />
         </svg>
-        <ul
-          class="menu menu-sm dropdown-content mt-3 z-[10] p-2 pop bg-base-100 rounded-box w-52"
-          tabindex="-1"
-        >
-          <li>
-            <a href="/challenges" class={isActive("/challenges")}>Challenges</a>
-          </li>
-          <li>
-            <a href="/scoreboard" class={isActive("/scoreboard")}>Scoreboard</a>
-          </li>
-          <li>
-            <a href="/teams" class={isActive("/teams")}>Teams</a>
-          </li>
-          {#if authState.isAdmin}
-            <li><a href="/admin" class={isActive("/admin")}>Admin Panel</a></li>
-          {/if}
-        </ul>
       </div>
+      <ul
+        class="menu menu-sm dropdown-content mt-3 z-[10] p-2 pop bg-base-100 rounded-box w-52"
+        tabindex="-1"
+      >
+        <li>
+          <a href="/challenges" class={isActive("/challenges")}>Challenges</a>
+        </li>
+        <li>
+          <a href="/scoreboard" class={isActive("/scoreboard")}>Scoreboard</a>
+        </li>
+        <li>
+          <a href="/teams" class={isActive("/teams")}>Teams</a>
+        </li>
+        {#if authState.isAdmin}
+          <li><a href="/admin" class={isActive("/admin")}>Admin Panel</a></li>
+        {/if}
+      </ul>
     </div>
     <a href="/" class="text-xl font-bold hidden lg:block">
       <div class="top-6 left-20 absolute flex flex-row gap-2 items-center">
