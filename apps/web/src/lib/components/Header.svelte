f2820ae252b496efd9c016cce960f66f18cffd76
tom <hi@tomn.me>
2025-07-13 09:08:26 +1000

    remove auth for public pages

--- apps/web/src/lib/components/Header.svelte
+++ apps/web/src/lib/components/Header.svelte
@@ -66,24 +66,22 @@
   </div>
 
   <div class="navbar-center hidden lg:block">
-    {#if authState.isAuthenticated}
-      <ul
-        class="menu menu-horizontal pop p-1 bg-base-100 rounded-lg flex gap-2 items-center"
-      >
-        <li>
-          <a href="/challenges" class={isActive("/challenges")}>Challenges</a>
-        </li>
-        <li>
-          <a href="/scoreboard" class={isActive("/scoreboard")}>Scoreboard</a>
-        </li>
-        <li>
-          <a href="/teams" class={isActive("/teams")}>Teams</a>
-        </li>
-        {#if authState.isAdmin}
-          <li><a href="/admin" class={isActive("/admin")}>Admin Panel</a></li>
-        {/if}
-      </ul>
-    {/if}
+    <ul
+      class="menu menu-horizontal pop p-1 bg-base-100 rounded-lg flex gap-2 items-center"
+    >
+      <li>
+        <a href="/challenges" class={isActive("/challenges")}>Challenges</a>
+      </li>
+      <li>
+        <a href="/scoreboard" class={isActive("/scoreboard")}>Scoreboard</a>
+      </li>
+      <li>
+        <a href="/teams" class={isActive("/teams")}>Teams</a>
+      </li>
+      {#if authState.isAdmin}
+        <li><a href="/admin" class={isActive("/admin")}>Admin Panel</a></li>
+      {/if}
+    </ul>
   </div>
 
   <div class="navbar-end">
@@ -91,64 +89,63 @@
       <a href="/auth" class="btn btn-primary px-6 sm:px-8 pop hover:pop"
         >Login</a
       >
-    {:else if authState.isAuthenticated}
-      <div class="dropdown dropdown-end">
+    {/if}
+    <div class="dropdown dropdown-end">
+      <div
+        tabindex="0"
+        role="button"
+        class="btn hover:pop pop flex flex-row gap-0 px-2 bg-base-100 items-center"
+        aria-label="User menu"
+      >
+        <Icon
+          icon="material-symbols:account-circle"
+          class="text-3xl lg:text-4xl text-neutral-600"
+        />
         <div
-          tabindex="0"
-          role="button"
-          class="btn hover:pop pop flex flex-row gap-0 px-2 bg-base-100 items-center"
-          aria-label="User menu"
+          class="hidden sm:flex flex-col items-start pl-2 lg:min-w-32 max-w-32 gap-0"
         >
-          <Icon
-            icon="material-symbols:account-circle"
-            class="text-3xl lg:text-4xl text-neutral-600"
-          />
           <div
-            class="hidden sm:flex flex-col items-start pl-2 lg:min-w-32 max-w-32 gap-0"
+            class="text-left text-sm lg:text-base font-medium w-full truncate"
           >
-            <div
-              class="text-left text-sm lg:text-base font-medium w-full truncate"
-            >
-              {authState.user?.name}
-            </div>
-            {#if authState.user?.team_name}
-              <div
-                class="text-left text-neutral-400 text-xs lg:text-sm hidden sm:block w-full truncate -mt-1"
-              >
-                {authState.user?.team_name}
-              </div>
-            {/if}
+            {authState.user?.name}
           </div>
-          <Icon icon="mdi:chevron-down" class="hidden sm:block ml-1" />
-        </div>
-        <ul
-          class="menu menu-sm dropdown-content mt-3 z-[10] p-1 px-2 pop bg-base-100 rounded-box w-52 gap-0" tabindex="-1"
-        >
-          <li class="flex flex-col sm:hidden pointer-events-none w-full gap-0">
-            <div class="w-full font-semibold truncate">
-              {authState.user?.name}
-            </div>
+          {#if authState.user?.team_name}
             <div
-              class="text-neutral-400 w-full font-semibold text-xs -mt-1 truncate"
+              class="text-left text-neutral-400 text-xs lg:text-sm hidden sm:block w-full truncate -mt-1"
             >
               {authState.user?.team_name}
             </div>
-          </li>
-          <div class="sm:hidden divider py-0 mt-1 mb-0"></div>
-          <li class="py-1">
-            <a href="/team" class={isActive("/team")}>Team</a>
-          </li>
-          <li class="py-1">
-            <a href="/settings" class={isActive("/settings")}>Settings</a>
-          </li>
-          <li>
-            <button
-              onclick={authState.logout}
-              class="text-error w-full text-left p-2">Logout</button
-            >
-          </li>
-        </ul>
+          {/if}
+        </div>
+        <Icon icon="mdi:chevron-down" class="hidden sm:block ml-1" />
       </div>
-    {/if}
+      <ul
+        class="menu menu-sm dropdown-content mt-3 z-[10] p-1 px-2 pop bg-base-100 rounded-box w-52 gap-0" tabindex="-1"
+      >
+        <li class="flex flex-col sm:hidden pointer-events-none w-full gap-0">
+          <div class="w-full font-semibold truncate">
+            {authState.user?.name}
+          </div>
+          <div
+            class="text-neutral-400 w-full font-semibold text-xs -mt-1 truncate"
+          >
+            {authState.user?.team_name}
+          </div>
+        </li>
+        <div class="sm:hidden divider py-0 mt-1 mb-0"></div>
+        <li class="py-1">
+          <a href="/team" class={isActive("/team")}>Team</a>
+        </li>
+        <li class="py-1">
+          <a href="/settings" class={isActive("/settings")}>Settings</a>
+        </li>
+        <li>
+          <button
+            onclick={authState.logout}
+            class="text-error w-full text-left p-2">Logout</button
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </div>
