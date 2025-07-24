a50f60eb1281df4649f01ecd858093473e531df7
joseph <joseph.surin@gmail.com>
2025-07-24 23:09:30 +1000

    add stats page to static, fix category stats table styling

---
+++ apps/web/src/routes/stats/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import StatsPage from "$lib/components/stats/page.svelte";
+  import { goto } from "$app/navigation";
+  import { IS_STATIC_EXPORT } from "$lib/static_export/middleware";
+
+  if (!IS_STATIC_EXPORT) {
+    goto("/");
+  }
+</script>
+
+<StatsPage canRefresh={false} />
