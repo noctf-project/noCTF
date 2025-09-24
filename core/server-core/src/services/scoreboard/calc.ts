c0f5de9c9eca75cd72ed3941f82bfc71e4bc5961
joseph <joseph.surin@gmail.com>
2025-09-24 20:23:59 +1000

    remove catch ()

--- core/server-core/src/services/scoreboard/calc.ts
+++ core/server-core/src/services/scoreboard/calc.ts
@@ -63,7 +63,7 @@ function ComputeScoresForChallenge(
       params,
       valid.filter(({ value }) => value === null).length,
     );
-  } catch (_) {
+  } catch {
     return {
       value: null,
       solves: [],
@@ -130,7 +130,7 @@ function ComputeScoreStreamForChallenge(
       valid.filter(({ value }) => value === null).length,
       true,
     );
-  } catch (_) {
+  } catch {
     return [];
   }
   const stream: { team_id: number; delta: number; updated_at: Date }[] = [];
