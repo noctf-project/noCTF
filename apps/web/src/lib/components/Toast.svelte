c02b36603e01a0f053c225af9ee6256ecf9a0bea
SkySourced <58579786+SkySourced@users.noreply.github.com>
2025-11-04 02:20:49 +1300

    Remove toast container div blocking link (#271)

--- apps/web/src/lib/components/Toast.svelte
+++ apps/web/src/lib/components/Toast.svelte
@@ -26,35 +26,37 @@
   const defaultStyle = toastStyles.info;
 </script>
 
-<div class="toast toast-bottom toast-center p-4 z-50 w-full max-w-xs">
-  {#each $toasts as toast (toast.id)}
-    {@const style = toastStyles[toast.type] || defaultStyle}
-    <div
-      role="alert"
-      transition:fly={{ duration: 350, x: 100, easing: quintOut }}
-      class="alert {style.class} shadow-lg flex items-center w-full max-w-xs sm:max-w-sm mt-2"
-    >
-      <Icon icon={style.icon} class="text-xl sm:text-2xl flex-shrink-0 mt-px" />
-
-      <p
-        class=" flex-1 whitespace-normal text-sm sm:text-base break-words mr-2 min-w-0"
+{#if $toasts.length > 0}
+  <div class="toast toast-bottom toast-center p-4 z-50 w-full max-w-xs">
+    {#each $toasts as toast (toast.id)}
+      {@const style = toastStyles[toast.type] || defaultStyle}
+      <div
+        role="alert"
+        transition:fly={{ duration: 350, x: 100, easing: quintOut }}
+        class="alert {style.class} shadow-lg flex items-center w-full max-w-xs sm:max-w-sm mt-2"
       >
-        {toast.message}
-      </p>
+        <Icon icon={style.icon} class="text-xl sm:text-2xl flex-shrink-0 mt-px" />
 
-      <button
-        aria-label="Close"
-        class="btn btn-xs btn-ghost btn-circle flex-shrink-0"
-        onclick={() => toasts.remove(toast.id)}
-      >
-        <Icon
-          icon="material-symbols:close-rounded"
-          class="text-lg sm:text-xl"
-        />
-      </button>
-    </div>
-  {/each}
-</div>
+        <p
+          class=" flex-1 whitespace-normal text-sm sm:text-base break-words mr-2 min-w-0"
+        >
+          {toast.message}
+        </p>
+
+        <button
+          aria-label="Close"
+          class="btn btn-xs btn-ghost btn-circle flex-shrink-0"
+          onclick={() => toasts.remove(toast.id)}
+        >
+          <Icon
+            icon="material-symbols:close-rounded"
+            class="text-lg sm:text-xl"
+          />
+        </button>
+      </div>
+    {/each}
+  </div>
+{/if}
 
 <style>
   .toast > :global(div.alert) {
